af414d513ee8e0bc98178dabcc38dcd9088c92c8
kjkleindorfer <kjkleindorfer@dmacc.edu>
2018-01-03 22:03:56 -0600

    File name changes

---
+++ src/model/ListItem.java
@@ -0,0 +1,64 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="items")
+public class ListItem {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="STORE")
+	private String store;
+	@Column(name="ITEM")
+	private String item;
+
+	public ListItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ListItem(String store, String item) {
+		this.store = store;
+		this.item = item;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "ListItem [id=" + id + ", store=" + store + ", item=" + item + "]";
+	}
+	
+	public String returnItemDetails() {
+		return store + ": " + item;
+	}
+
+}
